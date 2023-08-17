d830b41f8cfed4b23e02ace1f50a9702be418e2c
Chris Lyons <52037738+mephmanx@users.noreply.github.com>
2023-08-17 17:03:39 -0400

    update
    Signed-off-by: Chris Lyons <52037738+mephmanx@users.noreply.github.com>

--- orm.py
+++ orm.py
@@ -5,42 +5,21 @@ from sqlalchemy import String
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy_bigquery import DATETIME
 import config
-import json
-from sqlalchemy.ext.declarative import DeclarativeMeta
 from sqlalchemy.orm import registry
 
 mapper_registry = registry()
 
-class AlchemyEncoder(json.JSONEncoder):
-
-    def default(self, obj):
-        if isinstance(obj.__class__, DeclarativeMeta):
-            # an SQLAlchemy class
-            fields = {}
-            for field in [x for x in dir(obj) if not x.startswith('_') and x != 'metadata']:
-                data = obj.__getattribute__(field)
-                try:
-                    json.dumps(data) # this will fail on non-encodable values, like other classes
-                    fields[field] = data
-                except TypeError:
-                    fields[field] = None
-            # a json-encodable dict
-            return fields
-
-        return json.JSONEncoder.default(self, obj)
-
-
-Base = declarative_base()
-
-class POIData(Base):
+class POIData():
+    __table__ = config.TABLE_NAME
     pass
 
-class POIDeleteData(Base):
+class POIDeleteData():
+    __table__ = config.TABLE_NAME + "_deletes"
     pass
     
 mapper_registry.map_imperatively(POIData, Table(
        config.TABLE_NAME,
-        Base.metadata,
+        mapper_registry.metadata,
         Column("id", Integer, primary_key=True),
         Column("location", Geography("POINT")),
         Column("data", String),
@@ -50,7 +29,7 @@ mapper_registry.map_imperatively(POIData, Table(
 
 mapper_registry.map_imperatively(POIDeleteData, Table(
         config.TABLE_NAME + "_deletes",
-        Base.metadata,
+        mapper_registry.metadata,
         Column("id", Integer, primary_key=True),
         Column("account_id", String),
         Column("requested_on", DATETIME(timezone=True)),
